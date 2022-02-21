05e0496e87d15ab456c3776b360d804dc9e3ea3d
matheusjesse <matheus_jesse@hotmail.com>
2022-02-21 20:37:40 -0300

    rightPokedex componente

---
+++ my-pokedex/src/RightPokedex.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class RightPokedex extends React.Component {
+  render() {
+    return(
+      <div className="right-pokedex">
+
+      </div>
+    );
+  }
+}
+
+export default RightPokedex;
