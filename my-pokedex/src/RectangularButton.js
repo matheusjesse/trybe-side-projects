147b1d9f9a11434d0b52204bd8f9f2296462ecc2
matheusjesse <matheus_jesse@hotmail.com>
2022-02-21 20:34:24 -0300

    left pokedex rectangular button black

---
+++ my-pokedex/src/RectangularButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class RectangularButton extends React.Component {
+  render() {
+    return(
+      <p className="rect-button"></p>
+    ); 
+  }
+}
+
+export default RectangularButton;
