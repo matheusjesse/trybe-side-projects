6666a9d97f66cce2154234e0ed50e994c4d8f3b0
matheusjesse <matheus_jesse@hotmail.com>
2022-02-21 23:56:06 -0300

    css right-pokedex

--- my-pokedex/src/Pokedex.js
+++ my-pokedex/src/Pokedex.js
@@ -1,12 +1,14 @@
 import React from "react";
+import LeftPokedex from "./LeftPokedex";
 import RightPokedex from "./RightPokedex";
 
 class Pokedex extends React.Component {
   render() {
     return (
-      <>
+      <div className="pokedex-body">
+        <LeftPokedex />
         <RightPokedex />
-      </>
+      </div>
     );
   }
 }
