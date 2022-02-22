5c2eba6c6f0751f17ce68f16c99cca62c2db143d
matheusjesse <matheus_jesse@hotmail.com>
2022-02-22 00:31:35 -0300

    pokemon-selector flex

--- my-pokedex/src/PokemonCard.js
+++ my-pokedex/src/PokemonCard.js
@@ -4,7 +4,7 @@ class PokemonCard extends React.Component {
   render() {
     const {pokemonImage, pokemonName, pokemonType} = this.props;
     return(      
-      <div data-testid="pokemon-card">
+      <div className="pokemon-card" data-testid="pokemon-card">
         <img src={ pokemonImage } alt={ pokemonName } />
         <p data-testid="pokemon-name">{ pokemonName }</p>
         <p data-testid="pokemon-type">{ pokemonType }</p>
